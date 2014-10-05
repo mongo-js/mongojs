de4f9a44b6c40dc6aa0b5e35b596a2e9731b63b1
Eduardo Sorribas <eduardo@sorribas.org>
2014-10-05 18:03:52 +0200

    Ignore bulk tests in case db version < 2.6 (travis).

--- test/test-bulk-updates.js
+++ test/test-bulk-updates.js
@@ -9,30 +9,35 @@ insert('bulk', [{
 }, {
   name: 'Charmander', type: 'fire'
 }], function(db, t, done) {
-  var bulk = db.a.initializeOrderedBulkOp();
-  bulk.find({type: 'water'}).update({$set: {level: 1}});
-  bulk.find({type: 'water'}).update({$inc: {level: 2}});
-  bulk.insert({name: 'Spearow', type: 'flying'});
-  bulk.insert({name: 'Pidgeotto', type: 'flying'});
-  bulk.insert({name: 'Charmeleon', type: 'fire'});
-  bulk.find({type: 'flying'}).removeOne();
-  bulk.find({type: 'fire'}).remove();
-  bulk.find({type: 'water'}).updateOne({$set: {hp: 100}});
+  db.runCommand('serverStatus', function(err, resp) {
+    if (err) return t.fail();
+    if (parseFloat(resp.version) < 2.6) return t.end();
 
-  bulk.execute(function(err, res) {
-    t.ok(res.ok);
-    db.a.find(function(err, res) {
-      t.equal(res[0].name, 'Squirtle');
-      t.equal(res[1].name, 'Starmie');
-      t.equal(res[2].name, 'Lapras');
-      t.equal(res[3].name, 'Pidgeotto');
+    var bulk = db.a.initializeOrderedBulkOp();
+    bulk.find({type: 'water'}).update({$set: {level: 1}});
+    bulk.find({type: 'water'}).update({$inc: {level: 2}});
+    bulk.insert({name: 'Spearow', type: 'flying'});
+    bulk.insert({name: 'Pidgeotto', type: 'flying'});
+    bulk.insert({name: 'Charmeleon', type: 'fire'});
+    bulk.find({type: 'flying'}).removeOne();
+    bulk.find({type: 'fire'}).remove();
+    bulk.find({type: 'water'}).updateOne({$set: {hp: 100}});
 
-      t.equal(res[0].level, 3);
-      t.equal(res[1].level, 3);
-      t.equal(res[2].level, 3);
+    bulk.execute(function(err, res) {
+      t.ok(res.ok);
+      db.a.find(function(err, res) {
+        t.equal(res[0].name, 'Squirtle');
+        t.equal(res[1].name, 'Starmie');
+        t.equal(res[2].name, 'Lapras');
+        t.equal(res[3].name, 'Pidgeotto');
 
-      t.equal(res[0].hp, 100);
-      t.end();
+        t.equal(res[0].level, 3);
+        t.equal(res[1].level, 3);
+        t.equal(res[2].level, 3);
+
+        t.equal(res[0].hp, 100);
+        t.end();
+      });
     });
   });
 });
