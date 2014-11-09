bf75333d4700b26a6a1181c209e6288bb2b38954
Eduardo Sorribas <eduardo@sorribas.org>
2014-11-09 19:10:55 +0100

    Add test for cursor.rewind.

---
+++ test/test-cursor-rewind.js
@@ -0,0 +1,22 @@
+var insert = require('./insert');
+
+insert('remove', [{
+  name:'Squirtle', type:'water'
+}, {
+  name:'Starmie' , type:'water'
+}, {
+  name:'Lapras'  , type:'water'
+}], function(db, t, done) {
+  var cursor = db.a.find().sort({name: 1});
+  cursor.next(function(err, obj1) {
+    t.equal(obj1.name, 'Lapras')
+    cursor.next(function(err, obj2) {
+      t.equal(obj2.name, 'Squirtle')
+      cursor.rewind();
+      cursor.next(function(err) {
+        t.equal(obj1.name, 'Lapras')
+        t.end();
+      });
+    })
+  });
+});
