34076338abeed50bba16bc714bafac17f92d5a9e
Eduardo Sorribas <eduardo@sorribas.org>
2014-11-15 18:42:05 +0100

    updatedExisting on update return value doesn't exist anymore.

--- test/test-update.js
+++ test/test-update.js
@@ -5,7 +5,6 @@ insert('update', [{
 }], function(db, t, done) {
   db.a.update({hello:'world'}, {$set:{hello:'verden'}}, function(err, lastErrorObject) {
     t.ok(!err);
-    t.equal(lastErrorObject.updatedExisting, true);
     t.equal(lastErrorObject.n, 1);
 
     db.a.findOne(function(err, doc) {
