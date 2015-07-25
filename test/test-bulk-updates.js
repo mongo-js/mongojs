7b56b0fb42db2b7c86652054f82ba9483d8ef938
Thomas Watson Steen <w@tson.dk>
2015-07-25 12:49:07 +0200

    Remove spaces before colon in object literals

--- test/test-bulk-updates.js
+++ test/test-bulk-updates.js
@@ -23,7 +23,7 @@ insert('bulk', [{
     bulk.find({type: 'fire'}).remove()
     bulk.find({type: 'water'}).updateOne({$set: {hp: 100}})
 
-    bulk.find({name: 'Squirtle'}).upsert().updateOne({$set : {name: 'Wartortle', type: 'water'}})
+    bulk.find({name: 'Squirtle'}).upsert().updateOne({$set: {name: 'Wartortle', type: 'water'}})
     bulk.find({name: 'Bulbasaur'}).upsert().updateOne({$setOnInsert: {name: 'Bulbasaur'}, $set: {type: 'grass', level: 1}})
 
     bulk.execute(function (err, res) {
