f11bcd10abadef44a10a711f7c9a5a773a7c2540
saintedlama <christoph.walcher@gmail.com>
2015-07-26 13:20:19 +0200

    Merge options and connString + cols object into options object

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var Database = require('./lib/database')
 var bson = require('mongodb-core').BSON
+var xtend = require('xtend')
 
 module.exports = function (connString, cols, options) {
-  var db = new Database({connString: connString, cols: cols}, options)
+  var db = new Database(xtend({connString: connString, cols: cols}, options))
   if (typeof Proxy !== 'undefined') {
     var p = Proxy.create({
       get: function (obj, prop) {
