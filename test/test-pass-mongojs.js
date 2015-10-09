60297a3779612b84b18f4f62cf289f2328b01872
saintedlama <christoph.walcher@gmail.com>
2015-10-09 11:40:21 +0200

    Implement mongojs and mongodb-native connection passing

--- test/test-pass-mongojs.js
+++ test/test-pass-mongojs.js
@@ -1,13 +1,13 @@
 var test = require('./tape')
 var mongojs = require('../')
 
-test('receive a driver db or mongojs instance', function (t) {
+test('receive a mongojs instance', function (t) {
 
   var db = mongojs(mongojs('test', []), ['a'])
   var afterFind = function () {
     db.a.remove(function (err) {
       t.error(err)
-      t.equal(db.toString(), 'test')
+      t.equal(db.toString(), 'test', 'should expose database name')
 
       db.close(function (err) {
         t.error(err)
