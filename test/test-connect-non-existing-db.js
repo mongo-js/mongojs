48a00dfae513a3f37cb5d5890b7eb1114db50c26
saintedlama <christoph.walcher@gmail.com>
2015-10-09 12:02:58 +0200

    Add test to verify that connecting to a non existing database yields an error in the provided operation callback

---
+++ test/test-connect-non-existing-db.js
@@ -0,0 +1,16 @@
+var test = require('tape')
+var mongojs = require('../')
+
+test('connect non existing db', function (t) {
+  var db = mongojs('mongodb://1.0.0.1:23/aiguillage?connectTimeoutMS=100', ['a'])
+
+  db.runCommand({ping: 1}, function (err) {
+    t.ok(err, 'Should yield an error if non connection to db could be established')
+
+    db.runCommand({ping: 1}, function (err) {
+      t.ok(err, 'Should yield an error if non connection to db could be established')
+
+      t.end()
+    })
+  })
+})
