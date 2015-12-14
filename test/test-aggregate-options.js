f6b6f2efbbef40d7f76afd409a6f15394e1bdda7
saintedlama <christoph.walcher@gmail.com>
2015-12-14 10:47:20 +0100

    Add feature to pass options to aggregate. Closes #249

---
+++ test/test-aggregate-options.js
@@ -0,0 +1,18 @@
+var insert = require('./insert')
+
+insert('aggregate with options', [{
+  name: 'Squirtle', type: 'water'
+}, {
+  name: 'Starmie', type: 'water'
+}, {
+  name: 'Charmander', type: 'fire'
+}, {
+  name: 'Lapras', type: 'water'
+}], function (db, t) {
+  db.a.aggregate([{ $group: {_id: '$type'} }], { explain: true }, function (err, explained) {
+    t.error(err)
+    t.equal(explained[0].ok, 1)
+    t.ok(explained[0].stages)
+    t.end()
+  })
+})
