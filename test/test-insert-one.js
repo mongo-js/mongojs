5d33e91e6987a41fa92f813395aff66f9ffeae6f
saintedlama <christoph.walcher@gmail.com>
2019-10-20 22:25:32 +0200

    Test insertOne and not insert

--- test/test-insert-one.js
+++ test/test-insert-one.js
@@ -3,11 +3,11 @@ const mongojs = require('../index')
 const db = mongojs('test', ['a', 'b'])
 
 test('insertOne', (t) => {
-  db.a.insert({ name: 'Lapras' }, (err, doc) => {
+  db.a.insertOne({ name: 'Lapras' }, (err, doc) => {
     t.error(err)
     t.equal(doc.name, 'Lapras')
 
-    db.a.insert({ name: 'Pidgeotto' }, (err, doc) => {
+    db.a.insertOne({ name: 'Pidgeotto' }, (err, doc) => {
       t.error(err)
       t.equal(doc.name, 'Pidgeotto')
 
